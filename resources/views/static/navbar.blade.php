2f394077eb04f1100df850c99f39bdf45985f6c2
miguel huayhua <miguelhuayhuac2@gmail.com>
2023-03-08 17:45:03 -0400

    cambios realizados al sistema en SIE el 8 de marzo

---
+++ resources/views/static/navbar.blade.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg custom-navbar">
+  <a class="navbar-brand" href="#">
+<img src="./images/logorrnnii.png" class="custom-img mx-3 px-3" alt="">
+
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item custom-nav-item">
+      <i class="fa fa-home custom-icon"></i>
+           <a class="nav-link custom-link" href="#">Inicio</span></a>
+      </li>
+      <li class="nav-item custom-nav-item">
+        <a class="nav-link custom-link" href="#">Acerca De</a>
+      </li>
+     
+      <li class="nav-item custom-nav-item">
+        <a class="nav-link custom-link" href="#">Actividades</a>
+      </li>
+      <li class="nav-item custom-nav-item">
+        <a class="nav-link custom-link" href="#">Publicaciones</a>
+      </li>
+      <li class="nav-item custom-nav-item">
+        <a class="nav-link custom-link" href="#">Galeria</a>
+      </li>
+      <li class="nav-item custom-nav-item">
+        <a class="nav-link custom-link" href="#">Contactos</a>
+      </li>
+    </ul>
+   
+  </div>
+</nav>
