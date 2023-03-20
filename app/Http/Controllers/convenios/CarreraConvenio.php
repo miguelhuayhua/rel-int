639401abb8bc0418751f4eebd1113eac92ff61b3
miguel huayhua <miguelhuayhuac2@gmail.com>
2023-03-20 17:16:45 -0400

    cambios al apartado de convenios (nacionales, internacionales) el 20 de marzo en SIE

--- app/Http/Controllers/convenios/CarreraConvenio.php
+++ app/Http/Controllers/convenios/CarreraConvenio.php
@@ -10,21 +10,23 @@ class CarreraConvenio extends Controller
 {
     public function index($tipo, $id)
     {
+        $title = $tipo=='nacionales'?'Convenios Nacionales - UPEA':'Convenios Internacionales - UPEA';
         $carrera_conteo = null;
         $tipo == 'internacionales' ?
             $carrera_conteo = collect(DB::select('SELECT * FROM vista_convenios_internacionales WHERE id_carrera = ?', [$id]))->first() :
             $carrera_conteo = collect(DB::select('SELECT * FROM vista_convenios_nacionales WHERE id_carrera = ?', [$id]))->first();  //hace referencia al conteo de una carrera dado su id a la vez compara el tipo de convenio
         $detalles = DB::select(
             'SELECT * FROM  sic_convenio_carrera scc JOIN sic_convenio sc ON sc.id_convenios = scc.id_convenios
-            JOIN sic_tipo_convenio stc ON sc.id_tipo_convenio = stc.id_tipo_convenio WHERE scc.id_carrera = ? AND stc.nombre_tipo_convenio = ?',
-            [$id, ucfirst($tipo)]
+            JOIN sic_tipo_convenio stc ON sc.id_tipo_convenio = stc.id_tipo_convenio WHERE scc.id_carrera = ? AND stc.nombre_tipo_convenio = ? 
+            AND sc.estado = ?',
+            [$id, ucfirst($tipo),'Activo']
         ); // hace el listado de todos los convenios dado el id de la carrera y el tipo de convenio 'Nacionales' | 'Internacionales'
         return view(
             'convenios.carreraconvenio',
             [
                 'tipo' => $tipo,
                 'id' => $id,
-                'title' => $tipo,
+                'title' => $title,
                 'detalles' => $detalles,
                 'carrera_conteo' => $carrera_conteo
             ]
--- resources/views/convenios/carreraconvenio.blade.php
+++ resources/views/convenios/carreraconvenio.blade.php
@@ -9,8 +9,39 @@
                     {{ $carrera_conteo->nom_carrera }}
                 </h1>
             </div>
-            <div class="col-6">
+            <div class="col-12">
+                <ul class="list">
+                    @foreach ($detalles as $convenio)
+                        <li class="list-item">
+                            <div class="custom-item">
+                                <p class="publicacion">
+                                    Publicado el:
+                                    <span>
+                                        {{ $convenio->fecha_publicacion }}
+                                    </span>
+                                </p>
+                                <h3>
+                                    {{ $convenio->nombre_convenio }}
+                                </h3>
+                                <p>
+                                    <span>OBJETIVO:</span>
+                                    {{ $convenio->objetivo_convenio }}
+                                </p>
+                                <p>
+                                    <span>ENTIDAD:</span>
+                                    {{ $convenio->entidad }}
 
+                                </p>
+                                <a  href="{{ url('conveniosPdf/pdf1.pdf') }}"
+                                target="blank"
+                                    class="pdf">
+                                    <i class="fa fa-file-pdf-o" aria-hidden="true"></i>
+                                </a>
+                            </div>
+
+                        </li>
+                    @endforeach
+                </ul>
             </div>
         </div>
     </main>
